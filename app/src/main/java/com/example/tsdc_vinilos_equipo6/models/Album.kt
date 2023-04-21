84f4b2af0fd7fd6a115b7a944226c484dfb173cb
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-04-20 20:04:43 -0600

    Se integra tutorial MVVM

---
+++ app/src/main/java/com/example/tsdc_vinilos_equipo6/models/Album.kt
@@ -0,0 +1,11 @@
+package com.example.tsdc_vinilos_equipo6.models
+
+data class Album (
+    val albumId:Int,
+    val name:String,
+    val cover:String,
+    val releaseDate:String,
+    val description:String,
+    val genre:String,
+    val recordLabel:String
+)
---
+++ app/src/main/java/com/example/tsdc_vinilos_equipo6/models/Comment.kt
@@ -0,0 +1,7 @@
+package com.example.tsdc_vinilos_equipo6.models
+
+data class Comment (
+    val description:String,
+    val rating:String,
+    val albumId:Int
+)
