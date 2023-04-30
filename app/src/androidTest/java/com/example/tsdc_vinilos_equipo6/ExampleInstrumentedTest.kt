f40fdbf4dfd67b68d143e30a8af7fe7ac155854d
Jhon Guzman <jfguzman@indracompany.com>
2023-04-30 18:06:16 -0500

    Validar si esta desplegado el elemento

--- app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
@@ -5,6 +5,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -65,8 +66,8 @@ class ExampleInstrumentedTest {
 
         onView(withId(R.id.ArtistTitle)).check(matches(withText("Listado de Artistas")))
 
-        onView(withId(R.id.ArtistName)).check(matches(withText(FIRST_ITEM_ID)))
-        onView(withId(R.id.ArtisDate)).check(matches(withText(FIRST_DATE_ID)))
+        onView(withId(R.id.ArtistName)).check(matches(isDisplayed()))
+        onView(withId(R.id.ArtisDate)).check(matches(isDisplayed()))
 
 
     }
