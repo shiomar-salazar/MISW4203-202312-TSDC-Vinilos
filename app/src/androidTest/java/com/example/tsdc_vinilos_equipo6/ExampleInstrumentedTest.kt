a4e6f1b0188dc56a594534edbbc3dee5985b3270
Jhon Guzman <jfguzman@indracompany.com>
2023-04-30 16:28:43 -0500

    Se cargan PruebasE2e

--- app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
@@ -1,5 +1,8 @@
 package com.example.tsdc_vinilos_equipo6
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -20,5 +23,49 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tsdc_vinilos_equipo6", appContext.packageName)
+    }
+
+    fun btnMenuUsuarioClick(){
+        // click en Menu Usuario
+        onView(withId(R.id.MenuUserButton)).perform(click())
+    }
+    fun btnArtistCatalog(){
+        // click en Menu Usuario
+        onView(withId(R.id.ArtistUserButton)).perform(click())
+    }
+    fun btnAlbumCatalog(){
+        // click en Menu Usuario
+        onView(withId(R.id.AlbumUserButton)).perform(click())
+    }
+    fun btnCollectorCatalog(){
+        // click en Menu Usuario
+        onView(withId(R.id.ColecctionUserButton)).perform(click())
+    }
+    @Test
+    fun ArtistListTest(){
+        // click en Menu Usuario
+        btnMenuUsuarioClick()
+
+        //Click Catalogo de Artistas
+        btnArtistCatalog()
+    }
+
+
+    @Test
+    fun AlbumCatalogTest(){
+        // click en Menu Usuario
+        btnMenuUsuarioClick()
+
+        //Click Catalogo de Artistas
+        btnAlbumCatalog()
+    }
+
+    @Test
+    fun CollectorCatalogTest(){
+        // click en Menu Usuario
+        btnMenuUsuarioClick()
+
+        //Click Catalogo de Artistas
+        btnCollectorCatalog()
     }
 }
