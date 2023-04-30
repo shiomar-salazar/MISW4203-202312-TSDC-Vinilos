fe0b7f938c1d9a7d101bf25b9bc4e845d034a60e
Jhon Guzman <jfguzman@indracompany.com>
2023-04-30 16:39:25 -0500

    Se Agrega llamado Activity

--- app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/ExampleInstrumentedTest.kt
@@ -3,13 +3,14 @@ package com.example.tsdc_vinilos_equipo6
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.tsdc_vinilos_equipo6.ui.MainActivity
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,12 +19,17 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule
+    var mActivityTestRule = ActivityScenarioRule(
+        MainActivity::class.java
+    )
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tsdc_vinilos_equipo6", appContext.packageName)
     }
+
 
     fun btnMenuUsuarioClick(){
         // click en Menu Usuario
