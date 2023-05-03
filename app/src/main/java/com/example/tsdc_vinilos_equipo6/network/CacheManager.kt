ec516f6353979ac7809a9b91b403ebf18f0be1bd
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-05-03 09:07:53 -0600

    Se agrega cache para información de listado de Artistas, Coleccionistas, Albumes y Comentarios

---
+++ app/src/main/java/com/example/tsdc_vinilos_equipo6/network/CacheManager.kt
@@ -0,0 +1,61 @@
+package com.example.tsdc_vinilos_equipo6.network
+
+import android.content.Context
+import com.example.tsdc_vinilos_equipo6.models.Album
+import com.example.tsdc_vinilos_equipo6.models.Artist
+import com.example.tsdc_vinilos_equipo6.models.Collector
+import com.example.tsdc_vinilos_equipo6.models.Comment
+
+
+class CacheManager(context: Context) {
+    companion object{
+        var instance: CacheManager? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: CacheManager(context).also {
+                    instance = it
+                }
+            }
+    }
+
+    private var comments: HashMap<Int, List<Comment>> = hashMapOf()
+    private var albums: List<Album> = mutableListOf<Album>()
+    private var artists: List<Artist> = mutableListOf<Artist>()
+    private var collectors: List<Collector> = mutableListOf<Collector>()
+
+    fun addComments(albumId: Int, comment: List<Comment>){
+        if (!comments.containsKey(albumId)){
+            comments[albumId] = comment
+        }
+    }
+    fun getComments(albumId: Int) : List<Comment>{
+        return if (comments.containsKey(albumId)) comments[albumId]!! else listOf<Comment>()
+    }
+
+    fun addAlbums(newAlbums: List<Album>){
+        if (albums.isEmpty()){
+            albums = newAlbums
+        }
+    }
+    fun getAlbums() : List<Album>{
+        return if (albums.isEmpty()) listOf<Album>() else albums
+    }
+
+    fun addArtists(newArtists: List<Artist>){
+        if (artists.isEmpty()){
+            artists = newArtists
+        }
+    }
+    fun getArtists() : List<Artist>{
+        return if (artists.isEmpty()) listOf<Artist>() else artists
+    }
+
+    fun addCollectors(newCollectors: List<Collector>){
+        if (collectors.isEmpty()){
+            collectors = newCollectors
+        }
+    }
+    fun getCollectors() : List<Collector>{
+        return if (collectors.isEmpty()) listOf<Collector>() else collectors
+    }
+}
--- app/src/main/java/com/example/tsdc_vinilos_equipo6/repositories/CollectorsRepository.kt
+++ app/src/main/java/com/example/tsdc_vinilos_equipo6/repositories/CollectorsRepository.kt
@@ -1,12 +1,22 @@
 package com.example.tsdc_vinilos_equipo6.repositories
 
 import android.app.Application
-import com.android.volley.VolleyError
+import android.util.Log
 import com.example.tsdc_vinilos_equipo6.models.Collector
+import com.example.tsdc_vinilos_equipo6.network.CacheManager
 import com.example.tsdc_vinilos_equipo6.network.NetworkServiceAdapter
 
 class CollectorsRepository (val application: Application) {
-    suspend fun refreshData(): List<Collector>{
-        return NetworkServiceAdapter.getInstance(application).getCollectors()
+    suspend fun refreshData(): List<Collector> {
+        val potentialResp = CacheManager.getInstance(application.applicationContext).getCollectors()
+        return if(potentialResp.isEmpty()){
+            Log.d("Cache decision", "get from network")
+            val collector = NetworkServiceAdapter.getInstance(application).getCollectors()
+            CacheManager.getInstance(application.applicationContext).addCollectors(collector)
+            collector
+        } else{
+            Log.d("Cache decision", "return ${potentialResp.size} elements from cache")
+            potentialResp
+        }
     }
 }
