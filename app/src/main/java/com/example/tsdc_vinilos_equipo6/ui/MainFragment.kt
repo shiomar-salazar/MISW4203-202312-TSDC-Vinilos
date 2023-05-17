ef494360a6dfa253a7d321ed4ccff2886eebb99d
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-05-17 00:13:53 -0600

    Se agrega boton de Agregar comentario y album segun tipo de usuario

--- app/src/main/java/com/example/tsdc_vinilos_equipo6/ui/MainFragment.kt
+++ app/src/main/java/com/example/tsdc_vinilos_equipo6/ui/MainFragment.kt
@@ -1,15 +1,16 @@
 package com.example.tsdc_vinilos_equipo6.ui
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.tsdc_vinilos_equipo6.R
-
 
 
 class MainFragment : Fragment() {
@@ -23,16 +24,27 @@ class MainFragment : Fragment() {
             container, false
         )
 
+        val sharedPref = activity?.getSharedPreferences("myPref", MODE_PRIVATE)
+        val editor = sharedPref?.edit()
+
         val button = view.findViewById<View>(R.id.MenuUserButton) as Button
         button.setOnClickListener { v ->
+            editor?.apply {
+                putBoolean("isCollector",false)
+                apply()
+            }
             val action = MainFragmentDirections.actionMainFragmentToUserFragment()
             v?.findNavController()?.navigate(action)
         }
 
         val button2 = view.findViewById<View>(R.id.CollectorManuButton) as Button
-        button2.setOnClickListener { //val action = MainFragmentDirections.actionMainFragmentToUserFragment()
-            //v?.findNavController()?.navigate(action)
-            Toast.makeText(activity, "Not Implemented Yet", Toast.LENGTH_LONG).show()
+        button2.setOnClickListener { v ->
+            editor?.apply {
+                putBoolean("isCollector",true)
+                apply()
+            }
+            val action = MainFragmentDirections.actionMainFragmentToAlbumFragment(true)
+            v?.findNavController()?.navigate(action)
         }
         return view
     }
