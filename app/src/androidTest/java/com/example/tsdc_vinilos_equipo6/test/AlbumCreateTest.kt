86ae479278ada42d88674e0b45bd6d09a887fd92
Jhon Guzman <jfguzman@indracompany.com>
2023-05-20 16:13:33 -0500

    Se complementa prueba para para crear Album

--- app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/test/AlbumCreateTest.kt
+++ app/src/androidTest/java/com/example/tsdc_vinilos_equipo6/test/AlbumCreateTest.kt
@@ -1,26 +1,26 @@
 package com.example.tsdc_vinilos_equipo6.test
 
 import android.os.SystemClock
-import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.withClassName
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.withSpinnerText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.tsdc_vinilos_equipo6.R
 import com.example.tsdc_vinilos_equipo6.ui.MainActivity
+import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.CoreMatchers.endsWith
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.CoreMatchers.`is`
@@ -29,6 +29,8 @@ import org.hamcrest.core.AllOf.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -84,10 +86,14 @@ class AlbumCreateTest {
             )
         ).perform(
             click(),
-            typeText(valueToType)
+            typeText(valueToType),
+            closeSoftKeyboard()
         )
     }
 
+    /**
+     * Esta prueba tiene como objetivo crear un nuevo album
+     */
     @Test
     fun navegateToListAlbums() {
         //Click en Menu Usuario
@@ -105,21 +111,37 @@ class AlbumCreateTest {
 
     fun completeForm(){
         setTextLayoutViewByValue(R.id.name_album_textField,"Album prueba")
-        //Agregamos un tiempo de espera de 1000
+
         SystemClock.sleep(delayService2)
         setTextLayoutViewByValue(R.id.image_album_textField,"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com.mx%2Fgasrock23%2Fblack-sabbath%2F&psig=AOvVaw3_e-kyCDby8dsJEf2e-c_h&ust=1684554144715000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJivhKy7gP8CFQAAAAAdAAAAABAE")
-        //Agregamos un tiempo de espera de 1000
+
+        SystemClock.sleep(delayService2)
+        setTextLayoutViewByValue(R.id.descripcion_album_textField,"Album de prueba")
+
         SystemClock.sleep(delayService2)
         setTextViewByValue(R.id.date_album_datepicker,"01/01/2000")
-        //Agregamos un tiempo de espera de 1000
+
         SystemClock.sleep(delayService2)
+
         onView(withId(R.id.genero_album_spinner)).perform(click())
-        //Agregamos un tiempo de espera de 1000
+
         SystemClock.sleep(delayService2)
-        onData(allOf(withId(R.id.genero_album_spinner),`is`(instanceOf(String::class.java)),
-            `is`("Rock"))).perform(click())
-        //Agregamos un tiempo de espera de 1000
+
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Rock"))).perform(click())
+        onView(withId(R.id.genero_album_spinner)).check(matches(withSpinnerText(containsString("Rock"))))
+
+
         SystemClock.sleep(delayService2)
+        onView(withId(R.id.disquera_album_spinner)).perform(click())
+
+        SystemClock.sleep(delayService2)
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Discos Fuentes"))).perform(click())
+        onView(withId(R.id.disquera_album_spinner)).check(matches(withSpinnerText(containsString("Discos Fuentes"))))
+
+        SystemClock.sleep(delayService2)
+        onView(withId(R.id.album_create_button)).perform(click())
+        SystemClock.sleep(10000)
+
 
     }
 }
